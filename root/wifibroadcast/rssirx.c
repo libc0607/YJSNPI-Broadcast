a6e4f78fc220aac457976229d3f5b361fd5839cb
libc0607 <libc0607@gmail.com>
2019-11-02 22:59:35 +0800

    rssirx: working on new config

--- root/wifibroadcast/rssirx.c
+++ root/wifibroadcast/rssirx.c
@@ -1,4 +1,18 @@
 // rssirx by Rodizio. Based on wifibroadcast rx by Befinitiv. Licensed under GPL2
+// Mod by Github @libc0607
+//
+// Usage:
+//	rssirx ./config.ini
+//
+// config.ini:
+// [rssirx]
+// nic=wlan0
+// mode=0		#0-wifi, 1-udp, 2-both
+// udp_listen_port=20393
+// encrypt=0
+// password=1919810
+//
+
 /*
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -21,11 +35,8 @@
 #include <sys/resource.h>
 
 // We should use RTS for default.
-// Data frame: ether[0x00:1] == 0x08
-//#define PCAP_FILTER_CHAR "ether[0x00:2] == 0x0802 && ether[0x04:1] == 0xff"
 // RTS Frame: ether[0x00:1] == 0xB4
 #define PCAP_FILTER_CHAR "ether[0x00:2] == 0xB402 && ether[0x04:1] == 0xff"
-
 #define PROGRAM_NAME "rssirx"
 
 // this is where we store a summary of the information from the radiotap header
@@ -38,43 +49,43 @@ typedef struct  {
 } __attribute__((packed)) PENUMBRA_RADIOTAP_DATA;
 
 struct payloaddata_s {
+	// 0
 	int8_t signal;
-	uint32_t lostpackets;
 	int8_t signal_rc;
-	uint32_t lostpackets_rc;
 	uint8_t cpuload;
-	uint8_t temp;
-	uint32_t injected_block_cnt;
-	uint32_t skipped_fec_cnt;
-	uint32_t injection_fail_cnt;
-	long long injection_time_block;
-	uint16_t bitrate_kbit;
-	uint16_t bitrate_measured_kbit;
+	uint8_t temp;	
 	uint8_t cts;
 	uint8_t undervolt;
 	uint8_t cpuload_wrt;
 	uint8_t temp_wrt;
+	// 8
+	uint16_t bitrate_kbit;
+	uint16_t bitrate_measured_kbit;
+	uint32_t lostpackets;
+	// 16
+	uint32_t lostpackets_rc;
+	uint32_t injected_block_cnt;
+	uint32_t skipped_fec_cnt;
+	uint32_t injection_fail_cnt;
+	// 32
+	uint64_t injection_time_block;
+	uint64_t seqno;
+	// 48
 }  __attribute__ ((__packed__));
 
-int flagHelp = 0;
-int param_encrypt_enable = 0;
-char * param_encrypt_password = NULL;
-
-wifibroadcast_rx_status_t *rx_status = NULL;
-wifibroadcast_rx_status_t_rc *rx_status_rc = NULL;
-wifibroadcast_rx_status_t_sysair *rx_status_sysair = NULL;
-
 void usage(void) {
-	printf(
-	    PROGRAM_NAME" by Rodizio. Based on wifibroadcast rx by Befinitiv. Licensed under GPL2\n\n"
+	printf("\n"
+	    PROGRAM_NAME" by Rodizio. Based on wifibroadcast rx by Befinitiv. Licensed under GPL2\n"
+		"Mod by Github @libc0607\n\n"
 	    "Usage: "PROGRAM_NAME" <config.file>\n\n"
 	    "config example:\n"
 		"["PROGRAM_NAME"]\n"
 		"nic=wlan0\n"
+		"mode=0					# 0-wifi, 1-udp, 2-both\n"
+		"udp_bind_port=20393\n"
 		"encrypt=0\n"
-		"mode=0		#0-wifi, 1-udp, 2-both"
-		"udp_listen_port=20393 "
-		"password=1919810\n\n");
+		"password=1919810\n\n"
+	);
 	exit(1);
 }
 
@@ -131,7 +142,7 @@ void open_and_configure_interface(const char *name, monitor_interface_t *interfa
 
 // input: monitor_interface_t *interface
 // output: uint8_t * buf, size_t *len
-// receive packet (not decrypted) and save to *buf 
+// receive packet (not decrypted) and save to buf 
 int receive_packet(monitor_interface_t *interface, uint8_t * buf, size_t *len) 
 {
 	struct pcap_pkthdr * ppcapPacketHeader = NULL;
@@ -140,7 +151,6 @@ int receive_packet(monitor_interface_t *interface, uint8_t * buf, size_t *len)
 	u8 payloadBuffer[512];
 	u8 *pu8Payload = payloadBuffer;
 	int bytes, n, retval, u16HeaderLen;
-	struct payloaddata_s payloaddata;
 	
 	retval = pcap_next_ex(interface->ppcap, &ppcapPacketHeader,(const u_char**)&pu8Payload); 
 	if (retval < 0) {
@@ -166,7 +176,7 @@ int receive_packet(monitor_interface_t *interface, uint8_t * buf, size_t *len)
 										ppcapPacketHeader->len) < 0) 
 		exit(1);
 		
-	bzero(&prd, sizeof(prd));
+	bzero(&prd, sizeof(PENUMBRA_RADIOTAP_DATA));
 	while ((n = ieee80211_radiotap_iterator_next(&rti)) == 0) {
 		switch (rti.this_arg_index) {
 		case IEEE80211_RADIOTAP_FLAGS:
@@ -181,15 +191,28 @@ int receive_packet(monitor_interface_t *interface, uint8_t * buf, size_t *len)
 	return 0;
 }
 
+// input: udpfd, port, recv_len
+// output: uint8_t * buf, size_t *len
+// return: =recvfrom()
+// receive packet (not decrypted) and save to buf 
+int receive_packet_udp(int udpfd, struct sockaddr * addr, int recv_len, uint8_t * buf, size_t *len)
+{
+	int ret, slen = sizeof(addr);
+
+	ret = recvfrom(udpfd, buf, recv_len, 0, addr, (socklen_t *)&slen);
+	
+	return ret;
+}
+
 // input: raw_buffer, raw_length, encrypt_enable, 
 // output: decrypted_buffer, decryped_length
-int decrypt_payload(uint8_t * raw, size_t * raw_len, int en, char * pwd, 
+int decrypt_payload(uint8_t * raw, size_t raw_len, int en, char * pwd, 
 					uint8_t * buf, size_t * dec_len)
 {
 	uint8_t * p_dec_data;
 	int raw_exp_length, dec_length;		// expected length
 	
-	if (en == 0) {
+	if (en == 0 || pwd == NULL) {
 		memcpy(buf, raw, raw_len); 
 		*dec_len = raw_len;
 		return 0;
@@ -203,23 +226,30 @@ int decrypt_payload(uint8_t * raw, size_t * raw_len, int en, char * pwd,
 
 	raw_exp_length = ( (sizeof(struct payloaddata_s)/4) +2)*4;
 	if (raw_exp_length != raw_len) {
-		fprintf(stderr, "Warning: the packet length is not equal to that we need(got %d, expected %d). Maybe rssitx is not the same version?\n", raw_len, raw_exp_length);
+		fprintf(stderr, "Warning: the packet length is not equal to that we need(got %d, expected %d). Maybe rssitx is not the same version?\n", (int)raw_len, (int)raw_exp_length);
 	}
 	dec_length = (raw_exp_length > raw_len)? raw_len: raw_exp_length;
 	
-	p_dec_data = xxtea_decrypt(raw, dec_length, pwd, &dec_len);
-	memcpy(buf, p_dec_data, dec_len);
+	p_dec_data = xxtea_decrypt(raw, dec_length, pwd, dec_len);
+	memcpy(buf, p_dec_data, *dec_len);
 	free(p_dec_data);
 	
 	return 0;
 }
 
-// input: decrypted buffer
-// output: td
-int fill_buf_to_payload(uint8_t * buf, telemetry_data_t *td)
+// input: decrypted buffer, seqno (current)
+// output: seqno(new), td
+int fill_buf_to_payload(uint8_t * buf, uint64_t * seqno, telemetry_data_t *td)
 {
 	struct payloaddata_s * payload;
+	uint64_t seqno_get;
+	
 	payload = (struct payloaddata_s *)buf;
+	seqno_get = be64toh(payload->seqno);
+	
+	if (seqno_get < *seqno)
+		return 0;
+	*seqno = seqno_get;
 	
 	td->rx_status->adapter[0].current_signal_dbm = payload->signal;
 	td->rx_status->lost_packet_cnt = ntohl(payload->lostpackets);
@@ -311,16 +341,49 @@ void telemetry_init(telemetry_data_t *td)
 	td->sysair_status = status_memory_open_sysair();
 }
 
+// input: ini
+// output: addr
+int open_udp_binded_sock_by_conf(dictionary *ini, struct sockaddr_in * addr) 
+{
+	int udpfd;
+	
+	bzero(addr, sizeof(struct sockaddr_in));
+	addr->sin_family = AF_INET;
+	addr->sin_port = htons(atoi(iniparser_getstring(ini, PROGRAM_NAME":udp_bind_port", NULL)));
+	addr->sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if ((udpfd = socket(PF_INET, SOCK_DGRAM, 0)) == -1) 
+		printf("ERROR: Could not create UDP socket!");
+	if (-1 == bind(udpfd, (struct sockaddr*)addr, sizeof(addr))) {
+		fprintf(stderr, "Bind UDP port failed.\n");
+		exit(0);
+	}
+	
+	return udpfd;
+}
+
 int main(int argc, char *argv[]) 
 {
-
-	setpriority(PRIO_PROCESS, 0, 10);
-
-	monitor_interface_t iface;
-	int i;
+	int udpfd = 0, param_enc, param_mode;
+	char *param_pwd;
+	char *param_nic;
+	struct timeval to;
+	uint8_t raw[256], buf[256];
+	size_t raw_len, dec_len;
+	fd_set readset;
 	telemetry_data_t td;
+	monitor_interface_t iface;
+	struct sockaddr_in addr;
+	uint64_t seqno;
 	
-	if (argc !=2) {
+	setpriority(PRIO_PROCESS, 0, 10);
+	
+	bzero(raw, sizeof(raw));
+	bzero(buf, sizeof(buf));
+	bzero(&td, sizeof(telemetry_data_t));
+	bzero(&iface, sizeof(monitor_interface_t));
+
+	if (argc != 2) {
 		usage();
 	}
 	char *file = argv[1];
@@ -330,48 +393,49 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	
-	// support only one wi-fi interface; will be fixed later (¹¾¹¾¹¾)
-	open_and_configure_interface((char *)iniparser_getstring(ini, "rssirx:nic", NULL), 
-									interfaces + num_interfaces);
-	++num_interfaces;
-	usleep(10000); // wait a bit between configuring interfaces to reduce Atheros and Pi USB flakiness
-	
-	param_encrypt_enable = iniparser_getint(ini, "rssirx:encrypt", 0);
-	if (param_encrypt_enable == 1) {
-		param_encrypt_password = (char *)iniparser_getstring(ini, "rssirx:password", NULL);
+	param_mode = iniparser_getint(ini, PROGRAM_NAME":mode", 0);
+	if (param_mode == 0 || param_mode == 2) {
+		param_nic = (char *)iniparser_getstring(ini, PROGRAM_NAME":nic", NULL);
+		open_and_configure_interface(param_nic, &iface);
+		usleep(10000); // wait a bit 
 	}
+	if (param_mode == 1 || param_mode == 2) {
+		udpfd = open_udp_binded_sock_by_conf(ini, &addr);
+	}
+	
+	param_enc = iniparser_getint(ini, PROGRAM_NAME":encrypt", 0);
+	param_pwd = (param_enc == 1)? (char *)iniparser_getstring(ini, PROGRAM_NAME":password", NULL): NULL;
 	
 	telemetry_init(&td);
-	td.rx_status->wifi_adapter_cnt = num_interfaces;
-	td.rx_status_rc->wifi_adapter_cnt = num_interfaces;
-
-	int fd_sum = 0;
+	td.rx_status->wifi_adapter_cnt = 1;
+	td.rx_status_rc->wifi_adapter_cnt = 1;
+	seqno = 0;
+	
 	for (;;) {
-		fd_set readset;
-		struct timeval to;
-
 		to.tv_sec = 0;
 		to.tv_usec = 1e5;
-	
 		FD_ZERO(&readset);
-		for (i=0; i<num_interfaces; ++i) {
-			FD_SET(interfaces[i].selectable_fd, &readset);
-			fd_sum += interfaces[i].selectable_fd;
+		FD_SET(iface.selectable_fd, &readset);
+		FD_SET(udpfd, &readset);
+		
+		select(iface.selectable_fd+udpfd+1, &readset, NULL, NULL, &to);
+		if (FD_ISSET(iface.selectable_fd, &readset)) {
+			receive_packet(&iface, raw, &raw_len);
+			decrypt_payload(raw, raw_len, param_enc, param_pwd, buf, &dec_len);
+			fill_buf_to_payload(buf, &seqno, &td);
+			fprintf(stderr, "!");
 		}
-
-		int n = select(fd_sum+1, &readset, NULL, NULL, &to);
-
-		for (i=0; i<num_interfaces; ++i) {
-			if (n == 0) {
-//			    printf("n == 0\n");
-		    	    //break;
-			}
-			if (FD_ISSET(interfaces[i].selectable_fd, &readset)) {
-			    //result = process_packet(interfaces + i, i);
-				process_packet(interfaces + i, i);
-			}
+		if (FD_ISSET(udpfd, &readset)) {
+			if (0 >= receive_packet_udp(udpfd, (struct sockaddr *)&addr, sizeof(raw), raw, &raw_len))
+				continue;
+			decrypt_payload(raw, raw_len, param_enc, param_pwd, buf, &dec_len);
+			fill_buf_to_payload(buf, &seqno, &td);
+			fprintf(stderr, "?");
 		}
 	}
-
-	return (0);
+	
+	close(udpfd);
+	close(iface.selectable_fd);
+	iniparser_freedict(ini);
+	return 0;
 }
