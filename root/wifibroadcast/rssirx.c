76e8398f10014d1756fd5ae68f2226d78abff62f
libc0607 <libc0607@gmail.com>
2019-11-02 18:20:22 +0800

    rssirx: cleaning

--- root/wifibroadcast/rssirx.c
+++ root/wifibroadcast/rssirx.c
@@ -20,11 +20,11 @@
 #include <time.h>
 #include <sys/resource.h>
 
+// We should use RTS for default.
 // Data frame: ether[0x00:1] == 0x08
 //#define PCAP_FILTER_CHAR "ether[0x00:2] == 0x0802 && ether[0x04:1] == 0xff"
 // RTS Frame: ether[0x00:1] == 0xB4
 #define PCAP_FILTER_CHAR "ether[0x00:2] == 0xB402 && ether[0x04:1] == 0xff"
-// We should use RTS for default.
 
 #define PROGRAM_NAME "rssirx"
 
@@ -37,6 +37,24 @@ typedef struct  {
 	int m_nRadiotapFlags;
 } __attribute__((packed)) PENUMBRA_RADIOTAP_DATA;
 
+struct payloaddata_s {
+	int8_t signal;
+	uint32_t lostpackets;
+	int8_t signal_rc;
+	uint32_t lostpackets_rc;
+	uint8_t cpuload;
+	uint8_t temp;
+	uint32_t injected_block_cnt;
+	uint32_t skipped_fec_cnt;
+	uint32_t injection_fail_cnt;
+	long long injection_time_block;
+	uint16_t bitrate_kbit;
+	uint16_t bitrate_measured_kbit;
+	uint8_t cts;
+	uint8_t undervolt;
+	uint8_t cpuload_wrt;
+	uint8_t temp_wrt;
+}  __attribute__ ((__packed__));
 
 int flagHelp = 0;
 int param_encrypt_enable = 0;
@@ -48,12 +66,14 @@ wifibroadcast_rx_status_t_sysair *rx_status_sysair = NULL;
 
 void usage(void) {
 	printf(
-	    "rssirx by Rodizio. Based on wifibroadcast rx by Befinitiv. Licensed under GPL2\n\n"
-	    "Usage: rssirx <config.file>\n\n"
+	    PROGRAM_NAME" by Rodizio. Based on wifibroadcast rx by Befinitiv. Licensed under GPL2\n\n"
+	    "Usage: "PROGRAM_NAME" <config.file>\n\n"
 	    "config example:\n"
-		"[rssirx]\n"
+		"["PROGRAM_NAME"]\n"
 		"nic=wlan0\n"
-		"encrypt=1\n"
+		"encrypt=0\n"
+		"mode=0		#0-wifi, 1-udp, 2-both"
+		"udp_listen_port=20393 "
 		"password=1919810\n\n");
 	exit(1);
 }
@@ -63,7 +83,6 @@ typedef struct {
 	int selectable_fd;
 	int n80211HeaderLength;
 } monitor_interface_t;
-
 
 void open_and_configure_interface(const char *name, monitor_interface_t *interface) 
 {
@@ -110,234 +129,196 @@ void open_and_configure_interface(const char *name, monitor_interface_t *interfa
 	interface->selectable_fd = pcap_get_selectable_fd(interface->ppcap);
 }
 
-uint8_t process_packet(monitor_interface_t *interface, int adapter_no) {
+// input: monitor_interface_t *interface
+// output: uint8_t * buf, size_t *len
+// receive packet (not decrypted) and save to *buf 
+int receive_packet(monitor_interface_t *interface, uint8_t * buf, size_t *len) 
+{
 	struct pcap_pkthdr * ppcapPacketHeader = NULL;
 	struct ieee80211_radiotap_iterator rti;
 	PENUMBRA_RADIOTAP_DATA prd;
-	u8 payloadBuffer[100];
+	u8 payloadBuffer[512];
 	u8 *pu8Payload = payloadBuffer;
 	int bytes, n, retval, u16HeaderLen;
-
-	bzero(&prd, sizeof(prd));
-	
-	struct payloaddata_s {
-	    int8_t signal;
-	    uint32_t lostpackets;
-	    int8_t signal_rc;
-	    uint32_t lostpackets_rc;
-	    uint8_t cpuload;
-	    uint8_t temp;
-	    uint32_t injected_block_cnt;
-	    uint32_t skipped_fec_cnt;
-	    uint32_t injection_fail_cnt;
-	    long long injection_time_block;
-	    uint16_t bitrate_kbit;
-	    uint16_t bitrate_measured_kbit;
-	    uint8_t cts;
-	    uint8_t undervolt;
-		uint8_t cpuload_wrt;
-		uint8_t temp_wrt;
-	}  __attribute__ ((__packed__));
-
 	struct payloaddata_s payloaddata;
-
-	retval = pcap_next_ex(interface->ppcap, &ppcapPacketHeader,(const u_char**)&pu8Payload); // receive
-
+	
+	retval = pcap_next_ex(interface->ppcap, &ppcapPacketHeader,(const u_char**)&pu8Payload); 
 	if (retval < 0) {
 		if (strcmp("The interface went down",pcap_geterr(interface->ppcap)) == 0) {
-		    fprintf(stderr, "rx: The interface went down\n");
+		    fprintf(stderr, PROGRAM_NAME": The interface went down\n");
 		    exit(9);
 		} else {
-		    fprintf(stderr, "rx: %s\n", pcap_geterr(interface->ppcap));
+		    fprintf(stderr, PROGRAM_NAME": %s\n", pcap_geterr(interface->ppcap));
 		    exit(2);
 		}
 	}
-
 	if (retval != 1) 
 		return 0;
 
-	// fetch radiotap header length from radiotap header (seems to be 36 for Atheros and 18 for Ralink)
+	// fetch radiotap header length from radiotap header 
 	u16HeaderLen = (pu8Payload[2] + (pu8Payload[3] << 8));
-//	fprintf(stderr, "u16headerlen: %d\n", u16HeaderLen);
-//	fprintf(stderr, "ppcapPacketHeader->len: %d\n", ppcapPacketHeader->len);
 	if (ppcapPacketHeader->len < (u16HeaderLen + interface->n80211HeaderLength)) 
 		exit(1);
-
 	bytes = ppcapPacketHeader->len - (u16HeaderLen + interface->n80211HeaderLength);
-
 	if (bytes < 0) 
-		return(0);
+		return 0;
 	if (ieee80211_radiotap_iterator_init(&rti, (struct ieee80211_radiotap_header *)pu8Payload, 
 										ppcapPacketHeader->len) < 0) 
 		exit(1);
-
+		
+	bzero(&prd, sizeof(prd));
 	while ((n = ieee80211_radiotap_iterator_next(&rti)) == 0) {
 		switch (rti.this_arg_index) {
 		case IEEE80211_RADIOTAP_FLAGS:
 			prd.m_nRadiotapFlags = *rti.this_arg;
 			break;
-//		case IEEE80211_RADIOTAP_DBM_ANTSIGNAL:
-//			rx_status->adapter[adapter_no].current_signal_dbm = (int8_t)(*rti.this_arg);
-//			break;
 		}
 	}
-	pu8Payload += u16HeaderLen + interface->n80211HeaderLength;
 	
-	// decrypt if needed
-	uint8_t * p_write_data;
-	size_t write_data_length;
-	// decrypt length: min( (smallest value (bigger than sizeof(payloaddata) & divided by 4))+4,
-	//								bytes)
-	if (param_encrypt_enable) {
-		int decrypt_length = ( (sizeof(payloaddata)/4) +2)*4;
-		if (decrypt_length != bytes) {
-			fprintf(stderr, "Warning: the packet length is not equal to that we need(got %d, expected %d). Maybe rssitx is not the same version?\n", bytes, decrypt_length);
-		}
-		decrypt_length = (decrypt_length > bytes)? bytes: decrypt_length;
-		p_write_data = xxtea_decrypt(pu8Payload, decrypt_length, 
-								param_encrypt_password, &write_data_length);
-	} else {
-		p_write_data = pu8Payload;
-		write_data_length = sizeof(payloaddata);
-	}
-	// copy payloaddata (signal, lost packets count, cpuload, temp, ....) to struct
-	memcpy(&payloaddata, p_write_data, write_data_length); 
-	// free memory
-	if (param_encrypt_enable) {
-		free(p_write_data);
-	}
-
-//	printf ("signal:%d\n",payloaddata.signal);
-//	printf ("lostpackets:%d\n",payloaddata.lostpackets);
-//	printf ("signal_rc:%d\n",payloaddata.signal_rc);
-//	printf ("lostpackets_rc:%d\n",payloaddata.lostpackets_rc);
-//	printf ("cpuload:%d\n",payloaddata.cpuload);
-//	printf ("temp:%d\n",payloaddata.temp);
-//	printf ("injected_blocl_cnt:%d\n",payloaddata.injected_block_cnt);
-
-//	printf ("bitrate_kbit:%d\n",payloaddata.bitrate_kbit);
-//	printf ("bitrate_measured_kbit:%d\n",payloaddata.bitrate_measured_kbit);
-
-//	printf ("cts:%d\n",payloaddata.cts);
-//	printf ("undervolt:%d\n",payloaddata.undervolt);
-
-	rx_status->adapter[0].current_signal_dbm = payloaddata.signal;
-	rx_status->lost_packet_cnt = payloaddata.lostpackets;
-	rx_status_rc->adapter[0].current_signal_dbm = payloaddata.signal_rc;
-	rx_status_rc->lost_packet_cnt = payloaddata.lostpackets_rc;
-	rx_status_sysair->cpuload = payloaddata.cpuload;
-	rx_status_sysair->temp = payloaddata.temp;
-	rx_status_sysair->cpuload_wrt = payloaddata.cpuload_wrt;
-	rx_status_sysair->temp_wrt = payloaddata.temp_wrt;
-
-	rx_status_sysair->skipped_fec_cnt = payloaddata.skipped_fec_cnt;
-	rx_status_sysair->injected_block_cnt = payloaddata.injected_block_cnt;
-	rx_status_sysair->injection_fail_cnt = payloaddata.injection_fail_cnt;
-	rx_status_sysair->injection_time_block = payloaddata.injection_time_block;
-
-	rx_status_sysair->bitrate_kbit = payloaddata.bitrate_kbit;
-	rx_status_sysair->bitrate_measured_kbit = payloaddata.bitrate_measured_kbit;
-
-	rx_status_sysair->cts = payloaddata.cts;
-	rx_status_sysair->undervolt = payloaddata.undervolt;
-
-//	write(STDOUT_FILENO, pu8Payload, 18);
-	return(0);
+	pu8Payload += u16HeaderLen + interface->n80211HeaderLength;
+	buf = pu8Payload;
+	*len = bytes;
+	return 0;
 }
 
-void status_memory_init(wifibroadcast_rx_status_t *s) {
-	s->received_block_cnt = 0;
-	s->damaged_block_cnt = 0;
-	s->received_packet_cnt = 0;
-	s->lost_packet_cnt = 0;
-	s->tx_restart_cnt = 0;
-	s->wifi_adapter_cnt = 0;
-
-	int i;
-	for(i=0; i<8; ++i) {
-		s->adapter[i].received_packet_cnt = 0;
-		s->adapter[i].wrong_crc_cnt = 0;
-		s->adapter[i].current_signal_dbm = -126;
+// input: raw_buffer, raw_length, encrypt_enable, 
+// output: decrypted_buffer, decryped_length
+int decrypt_payload(uint8_t * raw, size_t * raw_len, int en, char * pwd, 
+					uint8_t * buf, size_t * dec_len)
+{
+	uint8_t * p_dec_data;
+	int raw_exp_length, dec_length;		// expected length
+	
+	if (en == 0) {
+		memcpy(buf, raw, raw_len); 
+		*dec_len = raw_len;
+		return 0;
 	}
-}
+	
+	// expected raw length for decrypt: 
+	//	min( 
+	//			(smallest value (bigger than sizeof(payloaddata) & divided by 4))+4,
+	//			(bytes)
+	//		)
 
-void status_memory_init_rc(wifibroadcast_rx_status_t_rc *s) {
-	s->received_block_cnt = 0;
-	s->damaged_block_cnt = 0;
-	s->received_packet_cnt = 0;
-	s->lost_packet_cnt = 0;
-	s->tx_restart_cnt = 0;
-	s->wifi_adapter_cnt = 0;
-
-	int i;
-	for(i=0; i<8; ++i) {
-		s->adapter[i].received_packet_cnt = 0;
-		s->adapter[i].wrong_crc_cnt = 0;
-		s->adapter[i].current_signal_dbm = -126;
+	raw_exp_length = ( (sizeof(struct payloaddata_s)/4) +2)*4;
+	if (raw_exp_length != raw_len) {
+		fprintf(stderr, "Warning: the packet length is not equal to that we need(got %d, expected %d). Maybe rssitx is not the same version?\n", raw_len, raw_exp_length);
 	}
+	dec_length = (raw_exp_length > raw_len)? raw_len: raw_exp_length;
+	
+	p_dec_data = xxtea_decrypt(raw, dec_length, pwd, &dec_len);
+	memcpy(buf, p_dec_data, dec_len);
+	free(p_dec_data);
+	
+	return 0;
 }
 
-void status_memory_init_sysair(wifibroadcast_rx_status_t_sysair *s) {
-	s->cpuload = 0;
-	s->temp = 0;
-	s->skipped_fec_cnt = 0;
-	s->injected_block_cnt = 0;
-	s->injection_fail_cnt = 0;
-	s->injection_time_block = 0;
-	s->bitrate_kbit = 0;
-	s->bitrate_measured_kbit = 0;
-	s->cts = 0;
-	s->undervolt = 0;
+// input: decrypted buffer
+// output: td
+int fill_buf_to_payload(uint8_t * buf, telemetry_data_t *td)
+{
+	struct payloaddata_s * payload;
+	payload = (struct payloaddata_s *)buf;
+	
+	td->rx_status->adapter[0].current_signal_dbm = payload->signal;
+	td->rx_status->lost_packet_cnt = ntohl(payload->lostpackets);
+	td->rx_status_rc->adapter[0].current_signal_dbm = payload->signal_rc;
+	td->rx_status_rc->lost_packet_cnt = ntohl(payload->lostpackets_rc);
+	td->sysair_status->cpuload = payload->cpuload;
+	td->sysair_status->temp = payload->temp;
+	td->sysair_status->cpuload_wrt = payload->cpuload_wrt;
+	td->sysair_status->temp_wrt = payload->temp_wrt;
+	td->sysair_status->skipped_fec_cnt = ntohl(payload->skipped_fec_cnt);
+	td->sysair_status->injected_block_cnt = ntohl(payload->injected_block_cnt);
+	td->sysair_status->injection_fail_cnt = ntohl(payload->injection_fail_cnt);
+	td->sysair_status->injection_time_block = be64toh(payload->injection_time_block);
+	td->sysair_status->bitrate_kbit = ntohs(payload->bitrate_kbit);
+	td->sysair_status->bitrate_measured_kbit = ntohs(payload->bitrate_measured_kbit);
+	td->sysair_status->cts = payload->cts;
+	td->sysair_status->undervolt = payload->undervolt;
+	
+	return 0;
 }
 
-wifibroadcast_rx_status_t *status_memory_open(void) {
+wifibroadcast_rx_status_t *status_memory_open(void) 
+{ 
 	char buf[128];
 	int fd;
+	
 	sprintf(buf, "/wifibroadcast_rx_status_uplink");
 	fd = shm_open(buf, O_RDWR, S_IRUSR | S_IWUSR);
-	if(fd < 0) { perror("shm_open"); exit(1); }
-	void *retval = mmap(NULL, sizeof(wifibroadcast_rx_status_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (retval == MAP_FAILED) { perror("mmap"); exit(1); }
+	if (fd < 0) { 
+		perror("shm_open"); 
+		exit(1); 
+	}
+	void *retval = mmap(NULL, sizeof(wifibroadcast_rx_status_t), 
+							PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (retval == MAP_FAILED) {
+		perror("mmap"); 
+		exit(1); 
+	}
 	wifibroadcast_rx_status_t *tretval = (wifibroadcast_rx_status_t*)retval;
-	status_memory_init(tretval);
 	return tretval;
 }
 
-wifibroadcast_rx_status_t_rc *status_memory_open_rc(void) {
+wifibroadcast_rx_status_t_rc *status_memory_open_rc(void) 
+{
 	char buf[128];
 	int fd;
+	
 	sprintf(buf, "/wifibroadcast_rx_status_rc");
 	fd = shm_open(buf, O_RDWR, S_IRUSR | S_IWUSR);
-	if(fd < 0) { perror("shm_open"); exit(1); }
-	void *retval = mmap(NULL, sizeof(wifibroadcast_rx_status_t_rc), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (retval == MAP_FAILED) { perror("mmap"); exit(1); }
+	if (fd < 0) { 
+		perror("shm_open"); 
+		exit(1); 
+	}
+	void *retval = mmap(NULL, sizeof(wifibroadcast_rx_status_t_rc), 
+						PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (retval == MAP_FAILED) { 
+		perror("mmap"); 
+		exit(1); 
+	}
 	wifibroadcast_rx_status_t_rc *tretval = (wifibroadcast_rx_status_t_rc*)retval;
-	status_memory_init_rc(tretval);
 	return tretval;
 }
 
-wifibroadcast_rx_status_t_sysair *status_memory_open_sysair(void) {
+wifibroadcast_rx_status_t_sysair *status_memory_open_sysair(void) 
+{
 	char buf[128];
 	int fd;
+	
 	sprintf(buf, "/wifibroadcast_rx_status_sysair");
 	fd = shm_open(buf, O_RDWR, S_IRUSR | S_IWUSR);
-	if(fd < 0) { perror("shm_open"); exit(1); }
-	void *retval = mmap(NULL, sizeof(wifibroadcast_rx_status_t_sysair), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (retval == MAP_FAILED) { perror("mmap"); exit(1); }
+	if (fd < 0) { 
+		perror("shm_open"); 
+		exit(1); 
+	}
+	void *retval = mmap(NULL, sizeof(wifibroadcast_rx_status_t_sysair), 
+						PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (retval == MAP_FAILED) { 
+		perror("mmap"); 
+		exit(1); 
+	}
 	wifibroadcast_rx_status_t_sysair *tretval = (wifibroadcast_rx_status_t_sysair*)retval;
-	status_memory_init_sysair(tretval);
 	return tretval;
+}
+
+void telemetry_init(telemetry_data_t *td) 
+{
+	td->rx_status = status_memory_open();
+	td->rx_status_rc = status_memory_open_rc();
+	td->sysair_status = status_memory_open_sysair();
 }
 
 int main(int argc, char *argv[]) 
 {
-	printf("RSSI RX started\n");
+
 	setpriority(PRIO_PROCESS, 0, 10);
 
-	monitor_interface_t interfaces[8];
-	int num_interfaces = 0;
+	monitor_interface_t iface;
 	int i;
-	//int result;
+	telemetry_data_t td;
 	
 	if (argc !=2) {
 		usage();
@@ -360,12 +341,10 @@ int main(int argc, char *argv[])
 		param_encrypt_password = (char *)iniparser_getstring(ini, "rssirx:password", NULL);
 	}
 	
-	rx_status = status_memory_open();
-	rx_status->wifi_adapter_cnt = num_interfaces;
-	rx_status_rc = status_memory_open_rc();
-	rx_status_rc->wifi_adapter_cnt = num_interfaces;
-	rx_status_sysair = status_memory_open_sysair();
-	
+	telemetry_init(&td);
+	td.rx_status->wifi_adapter_cnt = num_interfaces;
+	td.rx_status_rc->wifi_adapter_cnt = num_interfaces;
+
 	int fd_sum = 0;
 	for (;;) {
 		fd_set readset;
